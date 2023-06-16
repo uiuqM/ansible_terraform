005815d65be0f61d2af35812104b5a5718fbcd8a
uiuqM <wilque.coelho@mail.uft.edu.br>
2023-06-16 19:42:33 -0300

    initial settings.

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t3.small"
+  key_name = "con"
+  tags = {
+    Name = "Market project"
+  }
+}
